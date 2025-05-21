4f6bca1c0cab0d5874599c5b44e0e7b02a0a05d8
Laura Marques <161334577+Marquesaura@users.noreply.github.com>
2025-05-21 13:52:56 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+noStroke();
+
+background(81, 171, 178);
+fill(255, 177, 165); // rosa
+ellipse(60, 75, 100, 100);
+fill (121, 88, 51); //marrom
+ellipse(102, 25, 100, 100);
+fill (240, 223, 200); //bege
+ellipse(20, 26, 100, 100);
+rect (350,0,70,100,10);
